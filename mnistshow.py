c9abf2471521e1ddb5ffc14d79599c4ab42f0eef
jsrimr@naver.com <jsrimr@naver.com>
2018-03-22 16:35:29 +0900

    comment

--- mnistshow.py
+++ mnistshow.py
@@ -16,7 +16,19 @@ mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 img=mnist.train.images[0].reshape(28,28)
 plt.imshow(img,cmap="gray")
 
-#sess=tf.InteractiveSession()
-#
-#img=img.reshape(-1,28,28,1)
-#W1=tf.Variable(tf.random_normal([3,3,1,5],std))
+sess=tf.InteractiveSession()
+
+img=img.reshape(-1,28,28,1) #몇 장 가져오는지 모를때
+W1=tf.Variable(tf.random_normal([3,3,1,5],stddev=0.01))
+conv2d=tf.nn.conv2d(img,W1,strides=[1,1,1,1],padding="SAME")
+print(conv2d)
+
+sess.run(tf.global_variables_initializer())
+conv2d_img=conv2d.eval()
+print("Before size : ",conv2d_img.shape)
+conv2d_img=np.swapaxes(conv2d_img,0,3)
+print("After size : ",conv2d_img.shape)
+
+for i,one_img in enumerate(conv2d_img):
+    print("one_img shape= : ", one_img.shape)
+    plt.subplot(2,3,i+1),plt.imshow(one_img.reshape(28,28),cmap='gray')
